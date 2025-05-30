41de008fe065fc8e4b59c9d065cc78cd1bda9c10
Carlos Alexandre <carlos206819@gmail.com>
2025-05-30 16:38:37 -0300

    Refactor environment loading and route handling in index.php
    Moved dotenv initialization to index.php for centralized control, reducing redundancy in create.php and all.php. Added basic routing logic in index.php to handle /all, /create, and fallback to phpinfo().

--- create.php
+++ create.php
@@ -4,11 +4,6 @@ use Devc4rlos\SimpleBlog\Entity\Post;
 use Devc4rlos\SimpleBlog\Repository\Post\PdoPostRepository;
 use Devc4rlos\SimpleBlog\Service\PostService;
 
-require_once 'vendor/autoload.php';
-
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
-
 $postRepository = new PdoPostRepository();
 $postService = new PostService($postRepository);
 
--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
 
-phpinfo();
+require_once 'vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$uri = $_SERVER['REQUEST_URI'];
+
+if ($uri === '/all') {
+    require_once 'all.php';
+} else if ($uri === '/create') {
+    require_once 'create.php';
+} else {
+    phpinfo();
+}
